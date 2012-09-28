2efbe753be1d57ae45ac73b7268b75baadd4f384
Joe Hamman <jhamman@hydro.washington.edu>
2012-09-28 00:50:50 -0700

    Add index.php to git repo

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head> 
+<title>UW Land Surface Hydrology Research Group :: Joe Hamman</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+<link rel="stylesheet" type="text/css" href="/SurfaceWaterGroup/main.css"> 
+</head> 
+<body> 
+<div id="pageframe"> 
+  <div id="wrapper"> 
+    <?php include("/www/data/SurfaceWaterGroup/includes/header.inc"); ?> 
+    <div id="main"> 
+      <?php include("/www/data/SurfaceWaterGroup/includes/nav.inc"); ?> 
+    </div> 
+    <div id="right"> 
+      <!-- ***************  Main section of page --> 
+ <td align="right" valign="top" width="162">  
+        <h2 align="right"><img src="http://m4.licdn.com/mpr/mpr/shrink_150_150/p/2/000/024/1de/36fb7ba.jpg"  height=175 align="right" border="1" vspace=15 hspace=30></h2> 
+          </td> 
+       <h1>Joe Hamman</h1> 
+    <p>PhD Candidate<br> 
+       jhamman at u.washington.edu</p> 
+ 
+      <h2>Research Interests</h2> 
+ 
+      <p>Water Resources Management, Climate Change Impacts <br>  
+ 
+      <h2>Education</h2> 
+ 
+      <p><i>M.S., Civil Engineering, 2012<br>
+        University of Washington, Seattle, Washington
+      </i>            </p>
+<p><i>B.S., Civil &amp; Environmental Engineering, 2007<br>
+University of Arizona, Tucson, Arizona</i> 
+ 
+      <!-- ************End main section --> 
+    </p>
+    </div> 
+    <div id="clear">&nbsp;</div> 
+  </div> 
+  <?php include("/www/data/SurfaceWaterGroup/includes/footer.inc"); ?> 
+</div> 
+</body> 
+</html> 
+ 
+
