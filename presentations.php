7da639e26dd515a35f4ce3cc755d6789e209e273
Joe Hamman <jhamman@hydro.washington.edu>
2013-02-02 22:36:32 -0800

    minor revisions

--- presentations.php
+++ presentations.php
@@ -24,7 +24,9 @@
       <h2>Presentations</h2> 
  
       <ul>
-        <li>Hamman, J.J.(2012, October). Impacts on Flooding in the skagit river - Developing tools for better floodplain management. Oral presentation for King County Brownbag Seminar Series:  Responding to Climate Change, Seattle, WA. [<a 
+        <li>Hamman, J.J.(2012, October). Impacts on Flooding in the Skagit River - Developing tools for better floodplain management. Oral presentation for Washington Cooperative Fish and Wildlife Research Unit Seminar Series, Olympia, WA. [<a 
+href="http://www.hydro.washington.edu/~jhamman/presentations/Hamman_2012_WCFWRU_presentation.pdf">.pdf</a>] [<a href="http://www.hydro.washington.edu/~jhamman/presentations/Hamman_2012_WCFWRU_presentation.pptx">.pptx</a>]</li>
+        <li>Hamman, J.J.(2012, October). Impacts on Flooding in the Rkagit River - Developing tools for better floodplain management. Oral presentation for King County Brownbag Seminar Series:  Responding to Climate Change, Seattle, WA. [<a 
 href="http://www.hydro.washington.edu/~jhamman/presentations/Hamman_2012_KC_presentation.pdf">.pdf</a>] [<a href="http://www.hydro.washington.edu/~jhamman/presentations/Hamman_2012_KC_presentation.pptx">.pptx</a>]</li>
 	<li>Hamman, J.J.(2012, October). Planning for Climate Change and Flooding in the Lower Skagit River Basin. Oral presentation for URBDP 598C - Floodplain Management and Planning for River Communities, University of Washington, Seattle, WA. [<a 
 href="http://www.hydro.washington.edu/~jhamman/presentations/Hamman_2012_URBP598C_presentation.pdf">.pdf</a>] [<a href="http://www.hydro.washington.edu/~jhamman/presentations/Hamman_2012_URBP598C_presentation.pptx">.pptx</a>]</li>
@@ -36,11 +38,13 @@ Conference, Boise, ID. [<a href="http://www.hydro.washington.edu/~jhamman/presen
       <hr>
       <h2>Posters</h2>
       <ul>
+        <li>Nijssen, B., Hamman, J.J., Roberts, A., Tan, A., Lettenmaier, D.P. (2012, December). Land Surface Hydrology and the Land-Ocean Freshwater Flux in the Regional Arctic System Model (RASM). Poster session presented at the 2012 American Geophysical Union Fall Meeting, San Francisco, CA. [<a href="http://www.hydro.washington.edu/~jhamman/posters/Nijssen_2012_AGU_poster.pdf">.pdf</a>]</li>
+        <li>Hamman, J.J., Hamlet, A.F., Grossman, E.E., Fuller, R. (2012, December). Effects of Projected Twenty-First Century Sea Level Rise, Storm Surge, and River Flooding on Water Levels in the Skagit River Floodplain and Estuary. Poster session presented at the 2012 American Geophysical Union Fall Meeting, San Francisco, CA. [<a href="http://www.hydro.washington.edu/~jhamman/posters/Hamman_2012_AGU_poster.pdf">.pdf</a>]</li>
         <li>Hamman, J.J., Hamlet, A.F. (2012, April). Increases in Puget Sound Estuarine Flood Risk Under Climate Change. Poster session presented at the 2012 University of Washington Water Symposium, Seattle, WA. [<a href="http://www.hydro.washington.edu/~jhamman/posters/Hamman_2012_UWWS_poster.pdf">.pdf</a>]</li>
         <li>Hamman, J.J., Hamlet, A.F., Littell, J.  (2011, September).  Diagnosis of Changing Cool Season Precipitation Statistics in the Western U.S. from 1916-2003.  Poster session presented at the 2011 Pacific Northwest Climate Science Conference, Seattle, WA.  [<a href="http://www.hydro.washington.edu/~jhamman/posters/Hamman_2011_PNWCSC_poster.pdf">.pdf</a>]</li>
       </ul>
       <p>Updated: 
-        <!-- #BeginDate format:Am1 -->October 9, 2012<!-- #EndDate -->
+        <!-- #BeginDate format:Am1 -->December 2, 2012<!-- #EndDate -->
 
     </div> 
     <div id="clear">&nbsp;</div> 
